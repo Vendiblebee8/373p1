93c7f0efc868d3537ea9191a07230245b7d9704b
Vincenzo Cataneo <vcatane1@cs.binghamton.edu>
2023-09-14 01:04:51 -0400

    p1 done

---
+++ Makefile
@@ -0,0 +1,34 @@
+COURSE = cs373
+SEMESTER = fall2023
+
+# Update the CP_NUMBER if you adapt this makefile for future assignments
+CP_NUMBER = 1
+
+# Change the following to your own information
+LASTNAME = Cataneo
+GITUSERID = Vendiblebee8
+
+# The name of the executable
+EXE = cataneo_p1
+
+FLAGS = -Wall -w -Wextra -g
+# FLAGS = -Wall -O3
+CC = g++
+OBJ = obj
+
+# Store executables in a subdirectory
+all: $(EXE)
+
+$(EXE): main.o
+	$(CC) $(FLAGS) main.o -o $@
+
+main.o: main.cpp
+	$(CC) $(FLAGS) -c main.cpp -o $@
+
+
+tar:	clean
+	tar cvvf $(TARFILE) $(REPODIR)
+	gzip $(TARFILE)
+
+clean:
+	rm -f $(OBJ)/*.o $(EXE) *.tar.gz
--- main.cpp
+++ main.cpp
@@ -5,11 +5,21 @@
 #include <algorithm>
 #include <getopt.h>
 #include <stdlib.h>
-#include "parser.h"
 #include <vector>
+#include <map>
 #include <bits/stdc++.h>
+#include <sstream>
+#include <set>
+#include <unordered_set>
+#include <functional>
 
+using namespace std;
 
+vector<string> transitions;
+vector<string> endpoints;
+vector<string> accepts;
+int start;
+int local;
 
 bool fileOpen(string file, ifstream &fileObj)
 {
@@ -25,33 +35,111 @@ bool fileOpen(string file, ifstream &fileObj)
     }
 }
 
-void readFileS(string file, ifstream &fileObj)
-{
-    int setF = 0;
+int main(int argc, char *argv[]) {
+    int current_state = -1;
+    if (argc != 3) {
+        cerr << "Usage: " << argv[0] << " <input_file> <input_string>" << endl;
+        return 1;
+    }
+
+    ifstream inputFile(argv[1]);
+    if (!inputFile) {
+        cerr << "Failed to open input file: " << argv[1] << endl;
+        return 1;
+    }
+
+    string input = argv[2]; 
+
+    map<pair<int, char>, unordered_set<int>> transitionTable;
+
+    set<int> acceptStates;
+
+    auto addTransition = [&](int fromState, char input, int toState) {
+        transitionTable[make_pair(fromState, input)].insert(toState);
+    };
+
+    auto addAcceptState = [&](int state) {
+        acceptStates.insert(state);
+    };
+
+    auto isValidCharacter = [&](char c) {
+        return (isdigit(c) || isalpha(c));
+    };
+
     string line;
-    string name;
-    string buyP;
-    int setN;
-    string sellP;
-    while (getline(fileObj, line))
-    {
-        name = "";
-        buyP = -1;
-        setN = 0;
-        sellP = -1;
-        stringstream ss(line);
-        getline(ss, name, ' ');
-        getline(ss, sellP, ' ');
-        try
-        {
-            numS = +stoi(name);
-        }
-        catch (std::invalid_argument const &ex)
-        {
-            if (card.readS(name, sellP))
-            {
-                sell.push_back(card);
+    while (getline(inputFile, line)) {
+        istringstream iss(line);
+        string keyword;
+        iss >> keyword;
+
+        if (keyword == "state") {
+            int state;
+            string type1, type2;
+            iss >> state >> type1 >> type2;
+            if (type1 == "start" || type2 == "start") {
+                current_state = state;
             }
+            if (type1 == "accept" || type2 == "accept") {
+                addAcceptState(state);
+            }
+        } else if (keyword == "transition") {
+            int fromState, toState;
+            char inputChar;
+            iss >> fromState >> inputChar >> toState;
+            addTransition(fromState, inputChar, toState);
         }
     }
+
+    unordered_set<int> reachableStates;
+
+    function<void(int, const string&)> computeReachableStates;
+    computeReachableStates = [&](int state, const string& remainingInput) {
+        if (remainingInput.empty()) {
+            reachableStates.insert(state);
+            return;
+        }
+
+        char inputChar = remainingInput[0];
+        string remaining = remainingInput.substr(1);
+
+        auto transitions = transitionTable.find(make_pair(state, inputChar));
+        if (transitions != transitionTable.end()) {
+            for (int nextState : transitions->second) {
+                computeReachableStates(nextState, remaining);
+            }
+        }
+    };
+
+    computeReachableStates(current_state, input);
+
+    bool accepts = false;
+    for (int state : reachableStates) {
+        if (acceptStates.count(state) > 0) {
+            accepts = true;
+            break;
+        }
+    }
+
+    if (accepts) {
+        cout << "accept";
+        for (int state : acceptStates) {
+            if (reachableStates.count(state) > 0) {
+                cout << " " << state;
+            }
+        }
+        cout << endl;
+    } else {
+        if (reachableStates.empty()) {
+            cout << "reject" << endl;
+        } else {
+            cout << "reject";
+            for (int state : reachableStates) {
+                cout << " " << state;
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
 }
+
